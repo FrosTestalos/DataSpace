b083537f9fb78f0e46c2bb259201bfb303b1bbab
Frost2 <frostestalos@gmail.com>
2015-02-15 16:34:37 +0100

    fonction random pour pixel

--- DataSpace/src/com/frost/dataspace/graphics/Screen.java
+++ DataSpace/src/com/frost/dataspace/graphics/Screen.java
@@ -1,28 +1,45 @@
 package com.frost.dataspace.graphics;
 
+import java.util.Random;
+
 public class Screen {
-	private int width,height;
+	private int width, height;
 	public int[] pixels;
-	
-	int time=0;
-	int counter=0;
-	public Screen(int width, int height){
-		this.width=width;
-		this.height=height;
-		pixels=new int[width*height];
+
+	public int[] tiles = new int[64 * 64];
+	private Random random = new Random();
+
+	int xtime = 0;
+	int ytime = 50;
+	int counter = 0;
+
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height];
+
+		for (int i = 0; i < 64 * 64; i++) {
+			tiles[i] = random.nextInt(0xffffff);
+		}
+
 	}
-	public void clear(){
-		for(int i=0;i<pixels.length;i++){
-			pixels[i]=0;
+
+	public void clear() {
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
 		}
 	}
-	public void render(){
-		counter++;
-		if (counter%2==0){time++;}
-		for(int y=0;y<height;y++){
-			for (int x=0;x<width;x++){
-				pixels[time+188*width]=0xff00ff;
-			
+
+	public void render() {
+		for (int y = 0; y < height; y++) {
+			if (y < 0 || y >= height)
+				break;
+			for (int x = 0; x < width; x++) {
+				if (x < 0 || x >= width)
+					break;
+				int tileIndex=(x/32)+(y/32)*64;
+				pixels[x + y * width] =tiles[tileIndex];
+
 			}
 		}
 	}
